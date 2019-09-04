2551e5ba61c1d317330df56954fad33181277d79
evalor <mipone@foxmail.com>
2019-09-04 19:27:45 +0800

    二进制包

--- src/Package.php
+++ src/Package.php
@@ -1,37 +1,192 @@
 <?php
 
-
 namespace EasySwoole\Memcache;
-
 
 use EasySwoole\Spl\SplBean;
 
+/**
+ * BinaryPackage
+ * Class Package
+ * @package EasySwoole\Memcache
+ */
 class Package extends SplBean
 {
-    protected $command;
+    // for header
+    protected $magic;
+    protected $opCode;
+    protected $dataType;
+    protected $status;
+    protected $opaque;
+    protected $cas1;
+    protected $cas2;
+
+    // for body
+    protected $extras;
     protected $key;
-    protected $flags;
-    protected $exptime = 0;
-    protected $bytes = 0;
-    protected $data;
+    protected $value;
 
     /**
+     * Magic Getter
      * @return mixed
      */
-    public function getCommand()
+    public function getMagic()
     {
-        return $this->command;
+        return $this->magic;
     }
 
     /**
-     * @param mixed $command
+     * Magic Setter
+     * @param mixed $magic
+     * @return Package
      */
-    public function setCommand($command): void
+    public function setMagic($magic)
     {
-        $this->command = $command;
+        $this->magic = $magic;
+        return $this;
     }
 
     /**
+     * OpCode Getter
+     * @return mixed
+     */
+    public function getOpCode()
+    {
+        return $this->opCode;
+    }
+
+    /**
+     * OpCode Setter
+     * @param mixed $opCode
+     * @return Package
+     */
+    public function setOpCode($opCode)
+    {
+        $this->opCode = $opCode;
+        return $this;
+    }
+
+    /**
+     * DataType Getter
+     * @return mixed
+     */
+    public function getDataType()
+    {
+        return $this->dataType;
+    }
+
+    /**
+     * DataType Setter
+     * @param mixed $dataType
+     * @return Package
+     */
+    public function setDataType($dataType)
+    {
+        $this->dataType = $dataType;
+        return $this;
+    }
+
+    /**
+     * Status Getter
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Status Setter
+     * @param mixed $status
+     * @return Package
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    /**
+     * Opaque Getter
+     * @return mixed
+     */
+    public function getOpaque()
+    {
+        return $this->opaque;
+    }
+
+    /**
+     * Opaque Setter
+     * @param mixed $opaque
+     * @return Package
+     */
+    public function setOpaque($opaque)
+    {
+        $this->opaque = $opaque;
+        return $this;
+    }
+
+    /**
+     * Cas1 Getter
+     * @return mixed
+     */
+    public function getCas1()
+    {
+        return $this->cas1;
+    }
+
+    /**
+     * Cas1 Setter
+     * @param mixed $cas1
+     * @return Package
+     */
+    public function setCas1($cas1)
+    {
+        $this->cas1 = $cas1;
+        return $this;
+    }
+
+    /**
+     * Cas2 Getter
+     * @return mixed
+     */
+    public function getCas2()
+    {
+        return $this->cas2;
+    }
+
+    /**
+     * Cas2 Setter
+     * @param mixed $cas2
+     * @return Package
+     */
+    public function setCas2($cas2)
+    {
+        $this->cas2 = $cas2;
+        return $this;
+    }
+
+    /**
+     * Extras Getter
+     * @return mixed
+     */
+    public function getExtras()
+    {
+        return $this->extras;
+    }
+
+    /**
+     * Extras Setter
+     * @param mixed $extras
+     * @return Package
+     */
+    public function setExtras($extras)
+    {
+        $this->extras = $extras;
+        return $this;
+    }
+
+    /**
+     * Key Getter
      * @return mixed
      */
     public function getKey()
@@ -40,81 +195,90 @@ class Package extends SplBean
     }
 
     /**
+     * Key Setter
      * @param mixed $key
+     * @return Package
      */
-    public function setKey($key): void
+    public function setKey($key)
     {
         $this->key = $key;
+        return $this;
     }
 
     /**
+     * Value Getter
      * @return mixed
      */
-    public function getFlags()
+    public function getValue()
     {
-        return $this->flags;
+        return $this->value;
     }
 
     /**
-     * @param mixed $flags
+     * Value Setter
+     * @param mixed $value
+     * @return Package
      */
-    public function setFlags($flags): void
+    public function setValue($value)
     {
-        $this->flags = $flags;
+        $this->value = $value;
+        return $this;
+    }
+
+    public function unpack($binaryPackage)
+    {
+        $format = 'Cmagic/Copcode/nkeylength/Cextralength/Cdatatype/nstatus/Nbodylength/NOpaque/NCAS1/NCAS2';
+        $header = unpack($format, $binaryPackage);
+
+        $this->setCas1($header['CAS1']);
+        $this->setCas2($header['CAS2']);
+        $this->setMagic($header['magic']);
+        $this->setOpCode($header['opcode']);
+        $this->setStatus($header['status']);
+        $this->setOpaque($header['Opaque']);
+        $this->setDataType($header['datatype']);
+
+        // 除了头部还有其他的内容
+        if ($header['bodylength']) {
+            $bodyLength = $header['bodylength'];
+            $data = substr($binaryPackage, 24, $bodyLength);
+
+            // 解开额外信息
+            if ($header['extralength']) {
+                $extraUnpacked = unpack('Nint', substr($data, 0, $header['extralength']));
+                $this->extras = $extraUnpacked['int'];
+            }
+
+            $this->key = substr($data, $header['extralength'], $header['keylength']);
+            $this->value = substr($data, $header['extralength'] + $header['keylength']);
+        }
     }
 
     /**
-     * @return int
+     * 数据打包
+     * @return false|string
      */
-    public function getExptime(): int
+    public function __toString()
     {
-        return $this->exptime;
-    }
+        // 计算包各部分的长度
+        $keyLength = $this->getKey() ? strlen($this->getKey()) : 0x00;
+        $valueLength = $this->getValue() ? strlen($this->getValue()) : 0x00;
+        $extrasLength = $this->getExtras() ? strlen($this->getExtras()) : 0x00;
+        $totalBodyLength = $keyLength + $valueLength + $extrasLength;
 
-    /**
-     * @param int $exptime
-     */
-    public function setExptime(int $exptime): void
-    {
-        $this->exptime = $exptime;
-    }
+        // 打包数据为二进制包
+        $package = pack('CCnCCnNNNN', 0x80, $this->getOpCode(), $keyLength,
+            $extrasLength, null, null,
+            $totalBodyLength,
+            $this->getOpaque(),
+            $this->getCas1(),
+            $this->getCas2()
+        );
 
-    /**
-     * @return int
-     */
-    public function getBytes(): int
-    {
-        return $this->bytes;
-    }
+        $this->getExtras() && $package .= $this->getExtras();
+        $this->getKey() && $package .= $this->getKey();
+        $this->getValue() && $package .= $this->getValue();
 
-    /**
-     * @param int $bytes
-     */
-    public function setBytes(int $bytes): void
-    {
-        $this->bytes = $bytes;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getData()
-    {
-        return $this->data;
-    }
-
-    /**
-     * @param mixed $data
-     */
-    public function setData($data): void
-    {
-        $this->data = $data;
-    }
-
-    function __toString()
-    {
-        /*
-         * 这边返回命令string ,带\r\n
-         */
+        return $package;
     }
 }
